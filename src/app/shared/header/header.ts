c3f97eab92969f8730dcba20cec5a6ab7cf2d901
Luisferbillete <luisfernandovllamil@gmail.com>
2025-10-13 15:14:21 -0500

    Add header component with authentication check and integrated navbar and search

---
+++ src/app/shared/header/header.ts
@@ -0,0 +1,17 @@
+import { Component, computed, inject } from "@angular/core";
+import { Navbar } from "./components/navbar";
+import { Search } from "./components/search/search";
+import { LoginApi } from "@src/app/featured/auth/services/login-api";
+import { UserAuthentication } from "@src/app/featured/auth/services/user-authentication";
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.html',
+    imports: [Navbar, Search],
+})
+export class Header {
+    
+    private authService = inject(UserAuthentication);
+
+    isAuthenticated = computed(() => this.authService.getIsLoggedInSignal());
+}

10bc8541c327635f5848f36d47c00c89dd4645f9
Luisferbillete <luisfernandovllamil@gmail.com>
2025-10-13 15:12:39 -0500

    Add LoginApi service for user authentication and session management

---
+++ src/app/featured/auth/services/login-api.ts
@@ -0,0 +1,66 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { DestroyRef, inject, Injectable, signal, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { environment } from '@src/environments/environment';
+import { Router } from '@angular/router';
+import { catchError, EMPTY } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { UserAuthentication } from './user-authentication';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginApi {
+
+  private url = environment.apiUrl;
+  private httpClient = inject(HttpClient);
+  private router = inject(Router);
+  private userAuth = inject(UserAuthentication);
+  private platformId = inject(PLATFORM_ID);
+
+  private destroyRef = inject(DestroyRef);
+
+
+  loading = signal<boolean>(false);
+
+  errorResponse = signal<{error: boolean; message: string | null}>({ error: false, message: null });
+
+  loginWithCredentials(userName: string, password: string) {
+    this.loading.set(true);
+    return this.httpClient.post<{ accessToken: string, refreshToken: string }>(`${this.url}/Auth/login`, { userName, password })
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        catchError((error: HttpErrorResponse) => {
+          this.loading.set(false);
+          this.errorResponse.set({ error: true, message: error.error.message || 'Error del servidor' });
+          setTimeout(() => this.errorResponse.set({ error: false, message: null }), 3000);
+          return EMPTY;
+        })
+      )
+      .subscribe(response => {
+        this.errorResponse.set({ error: false, message: null });
+        this.loading.set(false);
+        this.userAuth.setTokenSignal(response.accessToken);
+        this.userAuth.setRefreshTokeSignal(response.refreshToken);
+        
+        if (isPlatformBrowser(this.platformId)) {
+          localStorage.setItem('tokenAccessLvJersey', response.accessToken);
+          localStorage.setItem('refreshTokenAccessLvJersey', response.refreshToken);
+        }
+        
+        this.router.navigate(['/']);
+      })
+    }
+  logout = () => {
+    this.userAuth.setTokenSignal('');
+    this.userAuth.setRefreshTokeSignal('');
+    
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem('tokenAccessLvJersey');
+      localStorage.removeItem('refreshTokenAccessLvJersey');
+    }
+    
+    this.router.navigate(['/login']);
+  }
+}
+

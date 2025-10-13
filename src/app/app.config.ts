ceafbce439bdd2758343fe261aa87c2e723952a2
Luisferbillete <luisfernandovllamil@gmail.com>
2025-10-13 15:10:27 -0500

    Refactor app configuration to include HTTP client and response interceptor; remove unused CSS file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,24 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { responseTransformInterceptor } from '@src/interceptors/responseTransformInterceptor';
+import { CORE_PROVIDERS } from './shared/services/service-registry';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), 
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        responseTransformInterceptor
+      ])
+    ),
+    provideClientHydration(
+      withEventReplay()
+    ),
+    ...CORE_PROVIDERS
   ]
 };

b8440531bf1c749cc2ffc80f814682cbbae44146
Luisferbillete <luisfernandovllamil@gmail.com>
2025-10-13 15:13:01 -0500

    Add decodeUserToken utility for decoding JWT tokens

---
+++ src/app/featured/auth/utilities/decodeToken.ts
@@ -0,0 +1,14 @@
+import { UserDecode } from "../model/userDecode";
+
+export const decodeUserToken = (token: string): UserDecode => {
+const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split('')
+      .map(c => '%' + c.charCodeAt(0).toString(16).padStart(2, '0'))
+      .join('')
+  );
+  return JSON.parse(jsonPayload);
+}

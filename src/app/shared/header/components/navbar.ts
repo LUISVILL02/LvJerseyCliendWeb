519e545084f07605dc89a5ca8453775b95e9a0bc
Luisferbillete <luisfernandovllamil@gmail.com>
2025-10-13 15:14:28 -0500

    Add navbar component with authentication check and theme toggle functionality

---
+++ src/app/shared/header/components/navbar.ts
@@ -0,0 +1,26 @@
+import { Component, inject, input } from "@angular/core";
+import { faCartShopping, faHeart, faMoon, faSun, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+import { ThemeService } from "../../theme/theme";
+import { ButtonGenericAction } from "../../components/button-generic-action/button-generic-action";
+import { commonRoutes } from "@src/app/app.routes";
+
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.html',
+    imports: [FaIconComponent, ButtonGenericAction],
+})
+export class Navbar {
+
+    faHeart = faHeart;
+    faCartShopping = faCartShopping;
+    faUser = faUser;
+    faSun = faSun;
+    faMoon = faMoon;
+
+    themeService = inject(ThemeService);
+
+    isAuthneticated = input<boolean>(false);
+
+    commonRoutes = commonRoutes;
+}

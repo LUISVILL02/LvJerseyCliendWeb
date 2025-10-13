76ce8c3f8375987f7c99a975b952de9f88188dc7
Luisferbillete <luisfernandovllamil@gmail.com>
2025-10-13 15:12:55 -0500

    Add UserAuthentication service for token management and user state handling

---
+++ src/app/featured/auth/services/user-authentication.ts
@@ -0,0 +1,39 @@
+import { computed, inject, Injectable, PLATFORM_ID, signal } from '@angular/core';
+import { UserDecode } from '../model/userDecode';
+import { decodeUserTokenAdapter } from '../adapters/authAdapter';
+import { decodeUserToken } from '../utilities/decodeToken';
+import { isPlatformBrowser } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserAuthentication {
+
+  private platformId = inject(PLATFORM_ID);
+
+  private userSignal = computed<UserDecode>(() => {
+    const token = this.tokenSignal();
+    if (token) return decodeUserTokenAdapter(decodeUserToken(token));
+    return {} as UserDecode;
+  });
+  private tokenSignal = signal<string>(isPlatformBrowser(this.platformId) ? localStorage.getItem('tokenAccessLvJersey') || '' : '');
+  private refreshTokeSignal = signal<string>(isPlatformBrowser(this.platformId) ? localStorage.getItem('refreshTokenAccessLvJersey') || '' : '');
+
+  private isLoggedInSignal = computed<boolean>(() => !!this.tokenSignal());
+
+
+  setTokenSignal = (token: string) => this.tokenSignal.set(token)
+  setRefreshTokeSignal = (refreshToken: string) => this.refreshTokeSignal.set(refreshToken)
+
+  setLocalStorageTokens = (accessToken: string, refreshToken: string) => {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('tokenAccessLvJersey', accessToken);
+      localStorage.setItem('refreshTokenAccessLvJersey', refreshToken);
+    }
+  }
+
+  getUserSignal = () => this.userSignal();
+  getTokenSignal = () => this.tokenSignal();
+  getRefreshTokeSignal = () => this.refreshTokeSignal();
+  getIsLoggedInSignal = () => this.isLoggedInSignal();
+}
